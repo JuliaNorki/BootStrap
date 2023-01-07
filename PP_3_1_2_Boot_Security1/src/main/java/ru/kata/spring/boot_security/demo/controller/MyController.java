efdf5caee631aa8fa26cb0dbb2d9a5f17853de78
JuliaNorki <109559162+JuliaNorki@users.noreply.github.com>
2023-01-07 13:53:32 -0800

    Add files via upload

---
+++ PP_3_1_2_Boot_Security1/src/main/java/ru/kata/spring/boot_security/demo/controller/MyController.java
@@ -0,0 +1,54 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+@RequestMapping("admin")
+public class MyController {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public MyController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+    @GetMapping()
+    public String showAdminPage(Model model, Principal principal) {
+        User userTest = userService.getUserByUsername(principal.getName());
+        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("roles", roleService.getRoles());
+        model.addAttribute("login", userTest);
+        return "adminPage";
+    }
+
+    @PutMapping("/{id}/update")
+    public String update(@ModelAttribute("user") User user,
+                         @PathVariable("id") Long id) {
+        userService.updateUser(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
+}
